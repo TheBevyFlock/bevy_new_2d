2eaca8d7489d73cb58d2fe521939045964d2023c
Ben Frankel <ben.frankel7@gmail.com>
2024-07-07 15:47:00 -0700

    Explain bevy plugin disabling thing

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -13,8 +13,10 @@ pub(super) fn plugin(app: &mut App) {
     app.add_plugins(
         DefaultPlugins
             .build()
+            // Add `AssetPlugin` via `asset::plugin`.
             .disable::<AssetPlugin>()
             .add_after::<AssetPlugin, _>(asset::plugin)
+            // Add `WindowPlugin` via `window::plugin`.
             .disable::<WindowPlugin>()
             .add_after::<WindowPlugin, _>(window::plugin),
     );
