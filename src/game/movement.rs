06f2194f19f02f664fb1be9d314a9791c63522f5
Ben Frankel <ben.frankel7@gmail.com>
2024-07-08 13:23:11 -0700

    Support arrow keys for movement (#76)

--- src/game/movement.rs
+++ src/game/movement.rs
@@ -13,32 +13,32 @@ pub(super) fn plugin(app: &mut App) {
 
 /// Handle keyboard input to move the player.
 fn handle_player_movement_input(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Velocity, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut player_velocity: Query<&mut Velocity, With<Player>>,
 ) {
     /// Since Bevy's default 2D camera setup is scaled such that
     /// one unit is one pixel, you can think of this as
     /// "How many pixels per second should the player move?"
     /// Note that physics engines may use different unit/pixel ratios.
     const SPEED: f32 = 240.0;
-    for mut velocity in query.iter_mut() {
-        velocity.0 = Vec3::ZERO;
 
-        if keyboard_input.pressed(KeyCode::KeyW) {
-            velocity.y += 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::KeyS) {
-            velocity.y -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::KeyA) {
-            velocity.x -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::KeyD) {
-            velocity.x += 1.0;
-        }
-
-        // Need to normalize and scale because otherwise
-        // diagonal movement would be faster than horizontal or vertical movement.
-        velocity.0 = velocity.normalize_or_zero() * SPEED;
+    let mut intent = Vec3::ZERO;
+    if input.pressed(KeyCode::KeyW) || input.pressed(KeyCode::ArrowUp) {
+        intent.y += 1.0;
     }
+    if input.pressed(KeyCode::KeyS) || input.pressed(KeyCode::ArrowDown) {
+        intent.y -= 1.0;
+    }
+    if input.pressed(KeyCode::KeyA) || input.pressed(KeyCode::ArrowLeft) {
+        intent.x -= 1.0;
+    }
+    if input.pressed(KeyCode::KeyD) || input.pressed(KeyCode::ArrowRight) {
+        intent.x += 1.0;
+    }
+    // Need to normalize and scale because otherwise
+    // diagonal movement would be faster than horizontal or vertical movement.
+    let intent = intent.normalize_or_zero();
+    let velocity = intent * SPEED;
+
+    player_velocity.single_mut().0 = velocity;
 }
