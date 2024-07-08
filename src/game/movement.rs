f4bdddc902875d1598c2a9761fb8f4014924222b
Jan Hohenheim <jan@hohenheim.ch>
2024-07-08 13:22:26 +0200

    Add velocity

--- src/game/movement.rs
+++ src/game/movement.rs
@@ -2,27 +2,40 @@ use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {}
 
-fn move_player(
-    time: Res<Time>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &Player)>,
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Component)]
+struct Player;
+
+#[derive(Debug, Clone, Copy, PartialEq, Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+fn handle_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Velocity, With<Player>>,
 ) {
-    for (mut transform, _player) in query.iter_mut() {
-        let mut direction = Vec3::ZERO;
-        if keyboard_input.pressed(KeyCode::W) {
-            direction.y += 1.0;
+    for mut velocity in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            velocity.y += 1.0;
         }
-        if keyboard_input.pressed(KeyCode::S) {
-            direction.y -= 1.0;
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            velocity.y -= 1.0;
         }
-        if keyboard_input.pressed(KeyCode::A) {
-            direction.x -= 1.0;
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            velocity.x -= 1.0;
         }
-        if keyboard_input.pressed(KeyCode::D) {
-            direction.x += 1.0;
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            velocity.x += 1.0;
         }
-        if direction.length() > 0.0 {
-            transform.translation += direction.normalize() * 4.0 * time.delta_seconds();
-        }
+    }
+}
+
+fn dampen_velocity(mut query: Query<&mut Velocity, With<Player>>) {
+    for mut velocity in query.iter_mut() {
+        velocity.0 *= 0.9;
+    }
+}
+
+fn apply_velocity(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity), With<Player>>) {
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation += Vec3::new(velocity.0.x, velocity.0.y, 0.0) * time.delta_seconds();
     }
 }
