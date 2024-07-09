55506377682a67640c5637d99f5650bfdc3b7867
Ben Frankel <ben.frankel7@gmail.com>
2024-07-08 21:34:26 -0700

    Pull `window` and `asset` code into `AppPlugin`

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
-mod asset;
 mod camera;
 #[cfg(feature = "dev")]
 mod dev;
 mod game;
 mod screen;
 mod util;
-mod window;
 
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 
 pub struct AppPlugin;
 
@@ -16,13 +14,24 @@ impl Plugin for AppPlugin {
         // Add Bevy plugins.
         app.add_plugins(
             DefaultPlugins
-                .build()
-                // Add `AssetPlugin` via `asset::plugin`.
-                .disable::<AssetPlugin>()
-                .add_after::<AssetPlugin, _>(asset::plugin)
-                // Add `WindowPlugin` via `window::plugin`.
-                .disable::<WindowPlugin>()
-                .add_after::<WindowPlugin, _>(window::plugin),
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics on web build on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Window {
+                        title: "bevy-template".to_string(),
+                        canvas: Some("#bevy".to_string()),
+                        fit_canvas_to_parent: true,
+                        prevent_default_event_handling: true,
+                        ..default()
+                    }
+                    .into(),
+                    ..default()
+                }),
         );
 
         // Add other plugins.
