cb22aef9587db62f8c2fcfb8e55ddabc28c7f502
Ben Frankel <ben.frankel7@gmail.com>
2024-07-08 21:26:29 -0700

    Flatten `src/core/` into `src/`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-mod core;
+mod asset;
+mod camera;
+#[cfg(feature = "dev")]
+mod dev;
 mod game;
 mod screen;
 mod util;
+mod window;
 
 use bevy::prelude::*;
 
@@ -9,6 +13,23 @@ pub struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((core::plugin, game::plugin, screen::plugin, util::plugin));
+        // Add Bevy plugins.
+        app.add_plugins(
+            DefaultPlugins
+                .build()
+                // Add `AssetPlugin` via `asset::plugin`.
+                .disable::<AssetPlugin>()
+                .add_after::<AssetPlugin, _>(asset::plugin)
+                // Add `WindowPlugin` via `window::plugin`.
+                .disable::<WindowPlugin>()
+                .add_after::<WindowPlugin, _>(window::plugin),
+        );
+
+        // Add other plugins.
+        app.add_plugins((game::plugin, screen::plugin, util::plugin, camera::plugin));
+
+        // Enable dev tools for dev builds.
+        #[cfg(feature = "dev")]
+        app.add_plugins(dev::plugin);
     }
 }
