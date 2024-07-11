d54ded0bd4403962241d2ea210cab7f8ded7038b
MiniaczQ <xnetroidpl@gmail.com>
2024-07-11 14:14:08 +0200

    init

--- src/screen/title.rs
+++ src/screen/title.rs
@@ -17,6 +17,9 @@ pub(super) fn plugin(app: &mut App) {
 enum TitleAction {
     Play,
     Credits,
+    /// Exit doesn't work well with embedded applications.
+    #[cfg(not(target = "wasm"))]
+    Exit,
 }
 
 fn enter_title(mut commands: Commands) {
@@ -26,18 +29,27 @@ fn enter_title(mut commands: Commands) {
         .with_children(|children| {
             children.button("Play").insert(TitleAction::Play);
             children.button("Credits").insert(TitleAction::Credits);
+
+            #[cfg(not(target = "wasm"))]
+            children.button("Exit").insert(TitleAction::Exit);
         });
 }
 
 fn handle_title_action(
     mut next_screen: ResMut<NextState<Screen>>,
     mut button_query: InteractionQuery<&TitleAction>,
+    mut app_exit: EventWriter<AppExit>,
 ) {
     for (interaction, action) in &mut button_query {
         if matches!(interaction, Interaction::Pressed) {
             match action {
                 TitleAction::Play => next_screen.set(Screen::Playing),
                 TitleAction::Credits => next_screen.set(Screen::Credits),
+
+                #[cfg(not(target = "wasm"))]
+                TitleAction::Exit => {
+                    app_exit.send(AppExit::Success);
+                }
             }
         }
     }
