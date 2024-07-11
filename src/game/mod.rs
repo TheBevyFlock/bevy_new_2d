8166aa13e2b03337227be8a3d7c8301b6faa726d
MiniaczQ <xnetroidpl@gmail.com>
2024-07-11 16:13:54 +0200

    fix fmt

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,10 +2,10 @@
 
 use bevy::prelude::*;
 
+pub mod assets;
 pub mod audio;
 mod movement;
 pub mod spawn;
-pub mod assets;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((audio::plugin, movement::plugin, spawn::plugin));
