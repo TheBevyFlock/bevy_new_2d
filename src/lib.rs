9be05f60d9d30d46f8715db7a2425eb57922ea3d
Jan Hohenheim <jan@hohenheim.ch>
2024-07-12 07:41:29 +0200

    Tweak some more

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ impl Plugin for AppPlugin {
                 })
                 .set(WindowPlugin {
                     primary_window: Window {
-                        title: "{{project-name}}".to_string(),
+                        title: "{{project-name | title_case}}".to_string(),
                         canvas: Some("#bevy".to_string()),
                         fit_canvas_to_parent: true,
                         prevent_default_event_handling: true,
