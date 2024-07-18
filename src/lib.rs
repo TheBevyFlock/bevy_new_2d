f15dadf9956a17ced6db0616845b81a7ea359391
Jan Hohenheim <jan@hohenheim.ch>
2024-07-18 21:36:05 +0200

    Update lib.rs
    Co-authored-by: Ben Frankel <ben.frankel7@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -80,8 +80,8 @@ fn spawn_camera(mut commands: Commands) {
         Camera2dBundle::default(),
         // Render all UI to this camera.
         // Not strictly necessary since we only use one camera,
-        // but if you don't use this component, your UI will disappear as soon
-        // as you add another camera. This includes indirect ways of adding cameras like using
+        // but if we don't use this component, our UI will disappear as soon
+        // as we add another camera. This includes indirect ways of adding cameras like using
         // [ui node outlines](https://bevyengine.org/news/bevy-0-14/#ui-node-outline-gizmos)
         // for debugging. So it's good to have this here for future-proofing.
         IsDefaultUiCamera,
