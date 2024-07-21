f633daa7f86efa21baf3e9c6d100a878e009a3b1
Jan Hohenheim <jan@hohenheim.ch>
2024-07-21 22:32:31 +0200

    Fix link

--- src/game/movement.rs
+++ src/game/movement.rs
@@ -1,7 +1,7 @@
 //! Handle player input and translate it into movement.
 //! Note that the approach used here is simple for demonstration purposes.
 //! If you want to move the player in a smoother way,
-//! consider using a [fixed timestep](https://github.com/bevyengine/bevy/blob/latest/examples/movement/physics_in_fixed_timestep.rs).
+//! consider using a [fixed timestep](https://github.com/bevyengine/bevy/blob/main/examples/movement/physics_in_fixed_timestep.rs).
 
 use bevy::{prelude::*, window::PrimaryWindow};
 
