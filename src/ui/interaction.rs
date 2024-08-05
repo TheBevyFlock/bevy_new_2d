0c5147794a67becd60e3c9a8ab92680ef4e51437
Jan Hohenheim <jan@hohenheim.ch>
2024-08-05 02:06:45 +0200

    Add docs to InteractionPalette (#224)

--- src/ui/interaction.rs
+++ src/ui/interaction.rs
@@ -7,7 +7,8 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(Update, (apply_interaction_palette, trigger_interaction_sfx));
 }
 
-/// Palette for widget interactions.
+/// Palette for widget interactions. Add this to an entity that supports [`Interaction`]s, such as a button,
+/// to change its [`BackgroundColor`] based on the current interaction state.
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct InteractionPalette {
