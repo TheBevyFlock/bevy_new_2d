e968cfb28c502426134d85ec76b89fecc166e098
Jan Hohenheim <jan@hohenheim.ch>
2024-08-06 22:18:57 +0200

    Move docs to module level

--- src/game/audio/sound_effects.rs
+++ src/game/audio/sound_effects.rs
@@ -2,6 +2,13 @@
 //! The main point of interest here is the extension trait `SoundEffectCommands`
 //! and its `play_sound_effect` method.
 //!
+//! This method accepts any type which implements `Into<String>`.
+//! This allows us to pass in `&str`, `String`, or a custom type that can be converted to a string.
+//!
+//! These custom types can be useful for defining enums that represent specific sound effects.
+//! Generally speaking, enum values should be used to represent one-off or special-cased sound effects,
+//! while string keys are a better fit for sound effects corresponding to objects loaded from a data file.
+//!
 //! This pattern is taken from the [Bevy example for sound effects](https://github.com/bevyengine/bevy/pull/14554).
 
 use bevy::{ecs::world::Command, prelude::*};
@@ -20,13 +27,6 @@ impl SoundEffectHandles {
     /// When defining the settings for this method, we almost always want to use [`PlaybackMode::Despawn`](bevy::audio::PlaybackMode).
     /// Every time a sound effect is played, a new entity is generated. Once the sound effect is complete,
     /// the entity should be cleaned up, rather than looping or sitting around uselessly.
-    ///
-    /// This method accepts any type which implements `AsRef<str>`.
-    /// This allows us to pass in `&str`, `String`, or a custom type that can be converted to a string.
-    ///
-    /// These custom types can be useful for defining enums that represent specific sound effects.
-    /// Generally speaking, enum values should be used to represent one-off or special-cased sound effects,
-    /// while string keys are a better fit for sound effects corresponding to objects loaded from a data file.
     fn play(&mut self, name: impl AsRef<str>, world: &mut World, settings: PlaybackSettings) {
         let name = name.as_ref();
         if let Some(sfx_list) = self.get_mut(name) {
