e65c1988cce73a14e2d9b2d649618e21285498c6
Jan Hohenheim <jan@hohenheim.ch>
2024-08-06 22:43:11 +0200

    Update src/demo/mod.rs
    Co-authored-by: Ben Frankel <ben.frankel7@gmail.com>

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -1,4 +1,4 @@
-//! Demo gameplay. All of these modules are intented do be here only for demonstration purposes
+//! Demo gameplay. All of these modules are only intended for demonstration purposes
 //! and should be replaced with your own game logic.
 //! Feel free to change the logic found here if you feel like tinkering around to get a feeling for the template.
 
