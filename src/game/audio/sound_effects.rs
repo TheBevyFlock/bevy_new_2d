cabeee5d8e6ea3f350cab88cda624961be500ce8
Jan Hohenheim <jan@hohenheim.ch>
2024-08-06 21:26:56 +0200

    Improve wording

--- src/game/audio/sound_effects.rs
+++ src/game/audio/sound_effects.rs
@@ -11,18 +11,18 @@ use crate::game::assets::SoundEffectHandles;
 
 pub(super) fn plugin(_app: &mut App) {
     // No setup required for this plugin.
-    // It's still good to have a function here so that you can add some setup later if needed.
+    // It's still good to have a function here so that we can add some setup later if needed.
 }
 
 impl SoundEffectHandles {
     /// Plays a random sound effect matching the given name.
     ///
-    /// When defining the settings for this method, you almost always want to use [`PlaybackMode::Despawn`](bevy::audio::PlaybackMode).
+    /// When defining the settings for this method, we almost always want to use [`PlaybackMode::Despawn`](bevy::audio::PlaybackMode).
     /// Every time a sound effect is played, a new entity is generated. Once the sound effect is complete,
     /// the entity should be cleaned up, rather than looping or sitting around uselessly.
     ///
     /// This method accepts any type which implements `AsRef<str>`.
-    /// This allows you to pass in `&str`, `String`, or a custom type that can be converted to a string.
+    /// This allows us to pass in `&str`, `String`, or a custom type that can be converted to a string.
     ///
     /// These custom types can be useful for defining enums that represent specific sound effects.
     /// Generally speaking, enum values should be used to represent one-off or special-cased sound effects,
@@ -30,8 +30,8 @@ impl SoundEffectHandles {
     fn play(&mut self, name: impl AsRef<str>, world: &mut World, settings: PlaybackSettings) {
         let name = name.as_ref();
         if let Some(sfx_list) = self.get_mut(name) {
-            // If you need precise control over the randomization order of your sound effects,
-            // store the RNG as a resource and modify these functions to take it as an argument.
+            // If we need precise control over the randomization order of our sound effects,
+            // we can store the RNG as a resource and modify these functions to take it as an argument.
             let rng = &mut rand::thread_rng();
             let random_sfx = sfx_list.choose(rng).unwrap();
 
@@ -54,18 +54,13 @@ struct PlaySoundEffect {
 
 impl Command for PlaySoundEffect {
     fn apply(self, world: &mut World) {
-        // Access both the world and the resource we need from it using resource_scope
-        // which temporarily removes the SoundEffects resource from the world
         world.resource_scope(|world, mut sound_effects: Mut<SoundEffectHandles>| {
             sound_effects.play(self.name, world, self.settings);
         });
     }
 }
 
-/// An "extension trait" used to make it convenient to play sound effects via [`Commands`].
-///
-/// This technique allows us to implement methods for types that we don't own,
-/// which can be used as long as the trait is in scope.
+/// An extension trait used to make it convenient to play sound effects via [`Commands`].
 pub trait SoundEffectCommands {
     fn play_sound_effect_with_settings(
         &mut self,
@@ -74,9 +69,6 @@ pub trait SoundEffectCommands {
     );
 
     fn play_sound_effect(&mut self, name: impl Into<String>) {
-        // This default method implementation saves work for types implementing this trait;
-        // if not overwritten, the trait's default method will be used here, forwarding to the
-        // more customizable method
         self.play_sound_effect_with_settings(name, PlaybackSettings::DESPAWN);
     }
 }
