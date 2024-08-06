264ea8689391071e399f9b7b7ed46ef8cc4776b5
Jan Hohenheim <jan@hohenheim.ch>
2024-08-06 22:45:42 +0200

    Run stable cargo fmt

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -1,6 +1,7 @@
-//! Demo gameplay. All of these modules are only intended for demonstration purposes
-//! and should be replaced with your own game logic.
-//! Feel free to change the logic found here if you feel like tinkering around to get a feeling for the template.
+//! Demo gameplay. All of these modules are only intended for demonstration
+//! purposes and should be replaced with your own game logic.
+//! Feel free to change the logic found here if you feel like tinkering around
+//! to get a feeling for the template.
 
 use bevy::prelude::*;
 
