487d87f39fc9f1b94d1884365c75bf38cfb83bd6
Ben Frankel <ben.frankel7@gmail.com>
2024-10-22 18:20:04 -0700

    Fix clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ enum AppSet {
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Name::new("Camera"),
-        Camera2d::default(),
+        Camera2d,
         // Render all UI to this camera.
         // Not strictly necessary since we only use one camera,
         // but if we don't use this component, our UI will disappear as soon
