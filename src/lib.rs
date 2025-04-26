390e75dafce87e8f8ce5036650ccb80b904746ee
Jan Hohenheim <jan@hohenheim.ch>
2025-04-26 21:54:44 +0200

    Remove unused import

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,7 @@ mod dev_tools;
 mod screens;
 mod theme;
 
-use bevy::{
-    asset::AssetMetaCheck,
-    audio::{AudioPlugin, Volume},
-    prelude::*,
-};
+use bevy::{asset::AssetMetaCheck, prelude::*};
 
 pub struct AppPlugin;
 
