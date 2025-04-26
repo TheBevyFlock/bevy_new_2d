93a7716ce668cdb3d40ed6ca286111f21d8fb8d6
Jan Hohenheim <jan@hohenheim.ch>
2025-04-27 00:28:43 +0200

    Make GlobalVolumeLabel reflect

--- src/screens/settings.rs
+++ src/screens/settings.rs
@@ -10,15 +10,13 @@ const MIN_VOLUME: f32 = 0.0;
 const MAX_VOLUME: f32 = 3.0;
 
 pub(super) fn plugin(app: &mut App) {
+    app.register_type::<GlobalVolumeLabel>();
     app.add_systems(OnEnter(Screen::Settings), spawn_settings_screen);
     app.add_systems(
         Update,
         update_volume_label.run_if(in_state(Screen::Settings)),
     );
 }
-
-#[derive(Component)]
-struct GlobalVolumeLabel;
 
 fn spawn_settings_screen(mut commands: Commands) {
     commands.spawn((
@@ -81,6 +79,10 @@ fn raise_volume(_: Trigger<Pointer<Click>>, mut global_volume: ResMut<GlobalVolu
     let new_factor = global_volume.volume.to_linear() + 0.1;
     global_volume.volume = Volume::Linear(new_factor.min(MAX_VOLUME));
 }
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+struct GlobalVolumeLabel;
 
 fn update_volume_label(
     mut label: Single<&mut Text, With<GlobalVolumeLabel>>,
