0ee1de15699b9240dc919ed8bc352298fa94540e
Jan Hohenheim <jan@hohenheim.ch>
2025-04-26 21:56:24 +0200

    Improve naming

--- src/screens/settings.rs
+++ src/screens/settings.rs
@@ -15,19 +15,19 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Settings), spawn_settings_screen);
     app.add_systems(
         Update,
-        update_music_volume_label.run_if(in_state(Screen::Settings)),
+        update_volume_label.run_if(in_state(Screen::Settings)),
     );
 }
 
 #[derive(Component)]
-struct MusicVolumeLabel;
+struct GlobalVolumeLabel;
 
 fn spawn_settings_screen(mut commands: Commands) {
     let music_settings = PercentageSettings {
-        name: "Music Volume".into(),
-        value_marker: MusicVolumeLabel,
-        on_minus: lower_music_volume,
-        on_plus: raise_music_volume,
+        name: "Audio Volume".into(),
+        value_marker: GlobalVolumeLabel,
+        on_minus: lower_volume,
+        on_plus: raise_volume,
         _marker: PhantomData,
     };
     commands.spawn((
@@ -107,21 +107,21 @@ where
     )
 }
 
-fn lower_music_volume(_: Trigger<Pointer<Click>>, mut music_factor: ResMut<GlobalVolume>) {
-    let new_factor = music_factor.volume.to_linear() - 0.1;
-    music_factor.volume = Volume::Linear(new_factor.max(MIN_VOLUME));
+fn lower_volume(_: Trigger<Pointer<Click>>, mut global_volume: ResMut<GlobalVolume>) {
+    let new_factor = global_volume.volume.to_linear() - 0.1;
+    global_volume.volume = Volume::Linear(new_factor.max(MIN_VOLUME));
 }
 
-fn raise_music_volume(_: Trigger<Pointer<Click>>, mut music_factor: ResMut<GlobalVolume>) {
-    let new_factor = music_factor.volume.to_linear() + 0.1;
-    music_factor.volume = Volume::Linear(new_factor.min(MAX_VOLUME));
+fn raise_volume(_: Trigger<Pointer<Click>>, mut global_volume: ResMut<GlobalVolume>) {
+    let new_factor = global_volume.volume.to_linear() + 0.1;
+    global_volume.volume = Volume::Linear(new_factor.min(MAX_VOLUME));
 }
 
-fn update_music_volume_label(
-    mut label: Single<&mut Text, With<MusicVolumeLabel>>,
-    music_factor: Res<GlobalVolume>,
+fn update_volume_label(
+    mut label: Single<&mut Text, With<GlobalVolumeLabel>>,
+    global_volume: Res<GlobalVolume>,
 ) {
-    let factor = music_factor.volume.to_linear();
+    let factor = global_volume.volume.to_linear();
     let percent = (factor * 100.0).round();
     let text = format!("{}%", percent);
     label.0 = text;
