99a9119af3ff484a56e419f95c67fb14f00094f8
Jan Hohenheim <jan@hohenheim.ch>
2025-04-27 01:06:45 +0200

    Fix removed comment

--- src/audio.rs
+++ src/audio.rs
@@ -12,6 +12,9 @@ pub fn music(handle: Handle<AudioSource>) -> impl Bundle {
     (AudioPlayer(handle), PlaybackSettings::LOOP, Music)
 }
 
+/// An organizational marker component that should be added to a spawned [`AudioPlayer`] if it's in the
+/// general "sound effect" category (e.g. footsteps, the sound of a magic spell, a door opening).
+///
 /// This can then be used to query for and operate on sounds in that category.
 #[derive(Component, Default)]
 pub struct SoundEffect;
