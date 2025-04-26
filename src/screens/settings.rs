0b7acb0cae9c85392d7964c7f484a6b3b1fec828
Jan Hohenheim <jan@hohenheim.ch>
2025-04-27 00:51:18 +0200

    Update src/screens/settings.rs
    Co-authored-by: Ben Frankel <ben.frankel7@gmail.com>

--- src/screens/settings.rs
+++ src/screens/settings.rs
@@ -10,8 +10,9 @@ const MIN_VOLUME: f32 = 0.0;
 const MAX_VOLUME: f32 = 3.0;
 
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<GlobalVolumeLabel>();
     app.add_systems(OnEnter(Screen::Settings), spawn_settings_screen);
+    
+    app.register_type::<GlobalVolumeLabel>();
     app.add_systems(
         Update,
         update_volume_label.run_if(in_state(Screen::Settings)),
