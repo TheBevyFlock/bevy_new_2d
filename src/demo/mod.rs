0860bf738b419aee380927c2281f3da29ffc932d
Jan Hohenheim <jan@hohenheim.ch>
2025-05-09 00:58:23 +0200

    Update src/demo/mod.rs
    Co-authored-by: Ben Frankel <ben.frankel7@gmail.com>

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -13,8 +13,8 @@ pub mod player;
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
         animation::plugin,
+        level::plugin,
         movement::plugin,
         player::plugin,
-        level::plugin,
     ));
 }
