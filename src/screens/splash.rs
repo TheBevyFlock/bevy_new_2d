d48a8e81877b80865387eb111265fe57a825bf88
Jan Hohenheim <jan@hohenheim.ch>
2025-09-14 21:34:48 +0200

    Update src/screens/splash.rs
    Co-authored-by: Ben Frankel <ben.frankel7@gmail.com>

--- src/screens/splash.rs
+++ src/screens/splash.rs
@@ -56,7 +56,7 @@ fn spawn_splash_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
             Name::new("Splash image"),
             Node {
                 margin: UiRect::all(Val::Auto),
-                width: percent(70.0),
+                width: percent(70),
                 ..default()
             },
             ImageNode::new(asset_server.load_with_settings(
